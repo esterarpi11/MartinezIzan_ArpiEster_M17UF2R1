600902def596691f5db60d69329609c464347c36
vejetto904 <83954743+vejetto904@users.noreply.github.com>
2023-11-28 19:34:38 +0100

    Primera versión para el script de movimiento de los enemigos

--- RogueLike/Assets/Scripts/EnemyBehavieour.cs
+++ RogueLike/Assets/Scripts/EnemyBehavieour.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyBehavieour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidad = 5f;  // Velocidad de persecución del enemigo
+    private Transform jugador;      // Referencia al transform del jugador
+
     void Start()
     {
-        
+        // Buscar el objeto con el tag "Player" al inicio del juego
+        jugador = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if (jugador == null)
+        {
+            Debug.LogError("No se encontró el objeto con el tag 'Player'.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (jugador != null)
+        {
+            // Calcular la dirección hacia el jugador
+            Vector3 direccion = jugador.position - transform.position;
+
+            // Normalizar la dirección para que el enemigo se mueva a una velocidad constante
+            direccion.Normalize();
+
+            // Mover al enemigo en la dirección del jugador
+            transform.Translate(direccion * velocidad * Time.deltaTime);
+        }
     }
 }
