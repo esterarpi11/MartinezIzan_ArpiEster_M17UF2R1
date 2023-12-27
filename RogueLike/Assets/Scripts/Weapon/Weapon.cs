26a0ffa3723272b3ad8034e27984f2219f5aab0a
vejetto904 <83954743+vejetto904@users.noreply.github.com>
2023-12-27 16:34:43 +0100

    Proyectil dispara

--- RogueLike/Assets/Scripts/Weapon/Weapon.cs
+++ RogueLike/Assets/Scripts/Weapon/Weapon.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject proyectilPrefab;  // Asigna el prefab del proyectil en el Inspector
+    public float velocidadProyectil = 10f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Detecta clic izquierdo del mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            DispararProyectil();
+        }
+    }
+
+    void DispararProyectil()
+    {
+        // Obtiene la posición del ratón en el mundo
+        Vector3 posicionRaton = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        posicionRaton.z = 0f;  // Asegura que la coordenada Z sea cero
+
+        // Calcula la dirección hacia la posición del ratón
+        Vector3 direccion = (posicionRaton - transform.position).normalized;
+
+        // Instancia el proyectil
+        GameObject proyectil = Instantiate(proyectilPrefab, transform.position, Quaternion.identity);
+
+        // Aplica fuerza al proyectil en la dirección calculada
+        Rigidbody2D rbProyectil = proyectil.GetComponent<Rigidbody2D>();
+        rbProyectil.velocity = direccion * velocidadProyectil;
+
+        // Destruye el proyectil después de un tiempo
+        Destroy(proyectil, 2f);
     }
 }
